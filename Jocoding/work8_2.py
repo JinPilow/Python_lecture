577b025d3291d728e45a9d2d0231a1381b42a844
JinPilow <80875572+JinPilow@users.noreply.github.com>
2021-04-04 13:28:32 +0900

    종합문제2

--- Jocoding/work8_2.py
+++ Jocoding/work8_2.py
@@ -70,13 +70,84 @@ for i in range(len(data)):
 tot = "".join(result)
 
 print(tot)
-'''
+
 
 #15
 #입력 예시: 0123456789 01234 01234567890 6789012345 012322456789
 #출력 예시: True False False True False
 
 data = input("숫잡를 입력하시오: ")
+result = []
 
-for i in range
+for i in data:
+    if i not in result:
+        result.append(i)
+    else:
+        print(False)
+if len(result) == 10:
+    print(True)
 
+
+#16
+dic = {
+    '.-':'A','-...':'B','-.-.':'C','-..':'D','.':'E','..-.':'F',
+    '--.':'G','....':'H','..':'I','.---':'J','-.-':'K','.-..':'L',
+    '--':'M','-.':'N','---':'O','.--.':'P','--.-':'Q','.-.':'R',
+    '...':'S','-':'T','..-':'U','...-':'V','.--':'W','-..-':'X',
+    '-.--':'Y','--..':'Z'
+}
+
+def read(s):
+    result = []
+    words = s.split("  ")
+    for word in words:
+        letter = word.split(" ")
+        for i in letter:
+            result.append(dic[i])
+        if not word == words[-1]:
+            result.append(" ")
+        sentence = "".join(result)
+    return sentence
+print(read(".... .  ... .-.. . . .--. ...  . .- .-. .-.. -.--"))
+
+
+#17
+import re
+
+p = re.compile("a[.]{3,}b")
+
+print (p.match("acccb"))    # None
+print (p.match("a....b"))   # 매치 객체 출력
+print (p.match("aaab"))     # None
+print (p.match("a.cccb"))   # None
+
+
+#18
+import re
+p = re.compile("[a-z]+")
+m = p.search("5 python")
+print(m.start() + m.end())
+
+
+#19
+import re
+
+s = """
+park 010-9999-9988
+kim 010-9909-7789
+lee 010-8789-7768
+"""
+
+p = re.compile("(\d{3}[-]\d{4})[-]\d{4}")
+result = p.sub("\g<1>-####", s)
+print(result)
+'''
+
+#20
+import re
+
+pat = re.compile(".*[@].*[.](?=com$|net$).*$")
+
+print(pat.match("pahkey@gmail.com"))
+print(pat.match("kim@daum.net"))
+print(pat.match("lee@myhome.co.kr"))
