f09a8d1eb02db684f05406ba648b5a5c210968c5
JinPilow <80875572+JinPilow@users.noreply.github.com>
2021-05-06 21:39:56 +0900

    beginning

--- Nadocoding/lesson1.py
+++ Nadocoding/lesson1.py
@@ -1,1 +1,8 @@
-print("Hello")
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+print("""우리집 """ + animal + """의 이름은""" + name + """입니다.
+{}는""" + str(age) + """살이며, 산책을 아주 좋아해요.
+{}는 어른일까요?""" + str(is_adult))
