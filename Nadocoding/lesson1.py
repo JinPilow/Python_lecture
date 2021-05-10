8484ee2251baa81540c983ede7c80d634514dc07
JinPilow <80875572+JinPilow@users.noreply.github.com>
2021-05-11 01:12:55 +0900

    quiz

--- Nadocoding/lesson1.py
+++ Nadocoding/lesson1.py
@@ -1,8 +1,19 @@
-animal = "강아지"
-name = "연탄이"
-age = 4
-hobby = "산책"
-is_adult = age >= 3
-print("""우리집 """ + animal + """의 이름은""" + name + """입니다.
-{}는""" + str(age) + """살이며, 산책을 아주 좋아해요.
-{}는 어른일까요?""" + str(is_adult))
+'''
+# Quiz 1
+
+station = ["사당","신도림","인청공항"]
+for i in station:
+    print(i + "행 열차가 들어오고 있습니다.")
+
+print(end="\n")
+
+for i in station:
+    print("{}행 열차가 들어오고 있습니다.".format(i))
+
+print(end="\n")
+
+for i in range(3):
+    print("%s행 열차가 들어오고 있습니다." %(station[i]))
+'''
+# Quiz 2
+print(eval("2+3**2*5"))
