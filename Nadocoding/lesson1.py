f7633732daa429494f8aea32719dc8e575d92533
JinPilow <80875572+JinPilow@users.noreply.github.com>
2021-05-16 00:20:50 +0900

    proceed

--- Nadocoding/lesson1.py
+++ Nadocoding/lesson1.py
@@ -44,7 +44,7 @@ for i in meeting_date:
 print("일 입니다.")
 print("오프라인 스터디 모임 날짜는 {}월 {}일로 선정되었습니다.".format(n, offline))
 '''
-
+'''
 # Quiz 3
 # 사이트별로 비밀번호를 만들어 주는 프로그램 작성
 # 예) http://naver.com
@@ -52,10 +52,19 @@ print("오프라인 스터디 모임 날짜는 {}월 {}일로 선정되었습니
 # 규칙 2 : 처음 만나본 점(.) 이후 부분은 제외 => naver
 # 규칙 3 : 남은 글자 중 처음 세자리 + 글자 갯수 + 글자 내 'e' 갯수 + "!"로 구성
 
-site = "http://naver.com"
+site = "http://google.com"
 index = site.index(".")
 site1 = site[7:index]
-print(site1)
 password = ""
 password = password + site1[:3] + str(len(site1)) + str(site1.count("e")) + "!"
 print(password)
+'''
+
+# Quiz 4
+# 20명 중 추첨을 통해 1명은 치킨, 3명은 커피 쿠폰 수령하게하는 프로그램 작성(중복 불가)
+
+from random import *
+
+applicants = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+a = randint(1, 20)
+if a in applicants:
