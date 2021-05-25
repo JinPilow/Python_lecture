6be85d7ad4970538a4ded50aede824e578142b29
JinPilow <80875572+JinPilow@users.noreply.github.com>
2021-05-25 16:38:12 +0900

    표준입출력

--- Nadocoding/lesson1.py
+++ Nadocoding/lesson1.py
@@ -131,4 +131,32 @@ print("키 {0}cm {1}의 표준 체중은 {2}kg 입니다.".format(height, gender
 # print("python", "Java", file = sys.stderr)
 # print("python", "Java", file = sys.stdout)
 
-scores = {}
+# # 왼쪽정렬, 오른쪽 정렬
+# scores = {"수학" : 0, "영어" : 50, "코딩" : 100}
+# for subject, score in scores.items():
+#     print(subject.ljust(8), str(score).rjust(4), sep=":")
+#
+# # 0 채우기 zfill
+# for num in range(1, 21):
+#     print("대기번호 : " + str(num).zfill(3))
+#
+# # input
+# answer = input("아무 값이나 입력하세요 : ")
+# print("입력하신 값은 {} 입니다".format(answer))
+
+# # 빈 자리는 빈 공간으로 두고, 오른쪽 정렬을 하되, 총 10자리 공간을 확보
+# print("{0: >10}".format(500))
+# # 양수일 땐 +로 표시, 음수일 땐 -로 표시
+# print("{0: >+10}".format(500))
+# # 왼쪽 정렬하고, 빈칸은 _로 채움
+# print("{0:_<+10}".format(500))
+# # 3자리 마다 콤마 찍어주기
+# print("{0:,}".format(100000000))
+# # 3자리 마다 콤마 찍어주기, 부호 추가
+# print("{0:+,}".format(100000000))
+# # 3자리 마다 콤마 찍어주기, 부호 추가, 자릿수 확보, 빈 공간은 ^로 채움
+# print("{0:^<+30,}".format(100000000))
+# # 소수점 출력
+# print("{0:f}".format(5/3))
+# # 소수점 특정 자리수까지만 표시
+# print("{0:.3f}".format(5/3))
