fbe4cc1b5ca4dadf4eed4024f3a28e79fad64a91
JinPilow <80875572+JinPilow@users.noreply.github.com>
2021-05-28 23:22:12 +0900

    파일 입출력

--- Nadocoding/lesson2.py
+++ Nadocoding/lesson2.py
@@ -82,36 +82,3 @@ import pickle
 #         report.write("부서 : \n")
 #         report.write("이름 : \n")
 #         report.write("업무 요약 : ")
-
-# # 클래스
-class Unit:
-    def __init__(self, name, hp, damage):
-        self.name = name
-        self.hp = hp
-        self.damage = damage
-        print("{0} 유닛이 생성되었습니다.".format(self.name))
-        print("체력 {0}, 공격력 {1}\n".format(self.hp, self.damage))
-
-    def attack(self, name, location, damage):
-        print("{0}: {1} 방향으로 적군을 공격합니다. [공격력 {2}]".format(name, location, damage))
-
-class AttackUnit:
-    def __init__(self, name, hp, damage):
-        self.name = name
-        self.hp = hp
-        self.damage = damage
-
-    def attack(self, location):
-        print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]".format(self.name, location, self.damage))
-
-    def damaged(self, damage):
-        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
-        self.hp -= damage
-        print("{0} : 현재 체력은 {1}입니다.".format(self.name, self.hp))
-        if self.hp <= 0:
-            print("{0} : 파괴되었습니다.".format(self.name))
-
-firebat1 = AttackUnit("파이어뱃", 50, 10)
-firebat1.attack("5시")
-firebat1.damaged(25)
-firebat1.damaged(25)
