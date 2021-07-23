d851ebcadc8870ac7db564aecc33c82b3cfff629
JinPilow <80875572+JinPilow@users.noreply.github.com>
2021-07-23 16:10:43 +0900

    정규식

---
+++ Nadocoding/Assignment/Practice3_Web_Scraping/webscraping_basic/4_re.py
@@ -0,0 +1,36 @@
+import re
+# ca?e의 4자리 알파벳 검색
+
+p = re.compile("ca.e")
+# . (ca.e): 하나의 문자를 의미 > case, cafe, care (O) | caffe (X)
+# ^ (^de): 문자열의 시작 > desk, destination (O) | fade (X)
+# $ (se$): 문자열의 끝 > case, base (O) | face(X)
+
+def print_match(m):
+    if m:
+        print("m.group() : ", m.group()) # 일치하는 문자열 반환
+        print("m.string : ", m.string) # 입력받은 문자열
+        print("m.start() : ", m.start()) # 일치하는 문자열의 시작 인덱스
+        print("m.end() : ", m.end()) # 일치하는 문자열의 끝 인덱스
+        print("m.span() : ", m.span()) # 일치하는 문자열의 시작 / 끝 인덱스
+    else:
+        print("매칭되지 않았습니다.")
+
+# m = p.match("careless") # match : 주어진 문자열의 처음부터 일치하는지 확인
+# print_match(m)
+
+# m = p.search("good care") # search : 주어진 문자열 중에 일치하는게 있는지 확인
+# print_match(m)
+
+# lst = p.findall("good care cafe") # findall : 일치하는 모든 값을 리스트 형태로 반환
+# print(lst)
+
+# 1. p = re.compile("원하는 형태")
+# 2. m = p.match("비교할 문자열") : 주어진 문자열의 처음부터 일치하는지 확인
+# 3. m = p.search("비교할 문자열") : 주어진 문자열 중에 일치하는게 있는지 확인
+# 4. lst = p.findall("비교할 문자열") : 일치하는 모든 것을 리스트 형태로 반환
+
+# 원하는 형태 : 정규식
+# . (ca.e): 하나의 문자를 의미 > case, cafe, care (O) | caffe (X)
+# ^ (^de): 문자열의 시작 > desk, destination (O) | fade (X)
+# $ (se$): 문자열의 끝 > case, base (O) | face(X)
