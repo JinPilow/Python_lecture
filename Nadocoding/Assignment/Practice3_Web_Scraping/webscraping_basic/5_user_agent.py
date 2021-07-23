9c40c09d3b6d43b071303db84cc023ec64bdd164
JinPilow <80875572+JinPilow@users.noreply.github.com>
2021-07-23 16:10:56 +0900

    User Agent

---
+++ Nadocoding/Assignment/Practice3_Web_Scraping/webscraping_basic/5_user_agent.py
@@ -0,0 +1,7 @@
+import requests
+url = "http://nadocoding.tistory.com"
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36"}
+res = requests.get(url, headers = headers)
+res.raise_for_status() # 오류가 생겼을 때 프로그램을 종료, 그 밑 코드는 실행되지 않음
+with open("nadocoding.html", "w", encoding = "utf8") as f:
+    f.write(res.text)
