631db6d111d133953b1f2ce35293d90c0b687e6d
JinPilow <80875572+JinPilow@users.noreply.github.com>
2021-08-01 00:20:18 +0900

    user_agent

--- Nadocoding/Assignment/Practice3_Web_Scraping/webscraping_basic/5_user_agent.py
+++ Nadocoding/Assignment/Practice3_Web_Scraping/webscraping_basic/5_user_agent.py
@@ -5,5 +5,4 @@ res = requests.get(url, headers = headers)
 res.raise_for_status() # 오류가 생겼을 때 프로그램을 종료, 그 밑 코드는 실행되지 않음
 with open("nadocoding.html", "w", encoding = "utf8") as f:
     f.write(res.text)
-
 # 권한이 없는 페이지에 접속할 때 사용. Google에 본인의 User-Agent 복사하여 붙여넣기
